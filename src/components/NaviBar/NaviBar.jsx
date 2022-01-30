eace8f775b7cde54b7798691c40c19a69af44f30
Konton022 <42418105+Konton022@users.noreply.github.com>
2022-01-30 22:42:51 +0500

    delete some files

--- src/components/NaviBar/NaviBar.jsx
+++ src/components/NaviBar/NaviBar.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const NaviBar = () => {
-	return (
-		<Navbar className="mb-3" bg="light" variant="light">
-    		<Container>
-    			<Navbar.Brand href="#home">Library of Books</Navbar.Brand>
-    			<Nav className="me-auto">
-      				<Nav.Link href="#home">Home</Nav.Link>
-      				<Nav.Link href="#contacts">Contacts</Nav.Link>
-    			</Nav>
-    		</Container>
-  		</Navbar>
-	);
-}
+    return (
+        <Navbar className='mb-3' bg='light' variant='light'>
+            <Container>
+                <Navbar.Brand href='#home'>Library of Books</Navbar.Brand>
+                <Nav className='me-auto'>
+                    <Nav.Link href='#home'>Home</Nav.Link>
+                    <Nav.Link href='#contacts'>Contacts</Nav.Link>
+                    <Nav.Link href='#'>Contacts</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+};
 
 export default NaviBar;
